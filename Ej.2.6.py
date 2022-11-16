c8c9ec70210884c9a3646e76c394bcd63b8638e1
Ruviixx <115063299+Ruviixx@users.noreply.github.com>
2022-11-16 22:50:12 +0100

    Delete Ej.2.6.py

--- Ej.2.6.py
+++
@@ -1,9 +0,0 @@
-'''Ejercicio 2.6
-Definir una función inversa() que calcule la inversión de una cadena. Por ejemplo la
-cadena "estoy probando" debería devolver la cadena "odnaborp yotse'''
-def inversa(texto):
-    textoinvertido=""
-    for i in texto:
-        textoinvertido=i+textoinvertido
-    return(textoinvertido)
-print(inversa("odnaborp yotse"))
